35434be24c147ace1d7cbc0f940bd494e8a13e2c
l-yohai <yohan9612@naver.com>
2021-03-23 10:55:39 +0900

    update

--- predict.py
+++ predict.py
@@ -5,7 +5,7 @@ def check_number(f, n):
     try:
         n = f(n)
     except:
-        exit("Value error")
+        exit("올바른 값이 아닙니다.")
     return n
 
 
@@ -16,12 +16,17 @@ if __name__ == '__main__':
                 theta = []
                 lines = f.readlines()
                 for x in lines:
-                    theta.append(check_number(float, x))
+                    check_number(float, x)
     except:
-        theta = [0, 0]
+        exit('output 파일에 올바르지 않은 값이 포함되어 있습니다.')
+    else:
+        for x in lines:
+            theta.append(check_number(float, x))
     print('Theta:', theta)
 
     d = input('차량의 주행거리를 입력하세요.\n')
+    if (d.isnumeric() is False or int(d) < 0):
+        exit('양의 정수만 입력해주세요.')
     d = check_number(float, d)
     p = theta[0] + theta[1] * d
     print('입력하신 차량의 예측 가격:', p)
